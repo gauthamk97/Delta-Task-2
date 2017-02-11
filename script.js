10b64a8722282b8ddfe3bbbb52a05cd655803d37
Gautham Kumar <heygautham@gmail.com>
2017-02-11 11:26:09 +0530

    Cloud X position works on resizing browser

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
-var platformHeight, cWidth = window.innerWidth, cHeight = window.innerWidth/2, cloudXPositions, cloudYPositions;
+var platformHeight, cWidth = window.innerWidth, cHeight = window.innerWidth/2, cloudXPositions, cloudYPositions, playerWidth, playerHeight, playerYPos=0;
+
+var yVel=0;
+var grav=0.6;
 
 cloudXPositions = [cWidth*0.1, cWidth*0.4, cWidth*0.65, cWidth*0.95];
 cloudYPositions = [cHeight*0.2, cHeight*0.1, cHeight*0.3, cHeight*0.15];
+var cloudLeftMove = [0,0,0,0];
 
 var i = 0;
 
@@ -19,34 +23,68 @@ function animate() {
 	cWidth = canvas.width;
 	cHeight = canvas.height;
 	
-	if (window.innerHeight >= canvas.height) {
-		canvas.style.marginTop=(window.innerHeight-canvas.height)/2 + "px"; //Centers canvas vertically	if browser is larger than canvas
+	if (window.innerHeight >= cHeight) {
+		canvas.style.marginTop=(window.innerHeight-cHeight)/2 + "px"; //Centers canvas vertically	if browser is larger than canvas
 	}
 	else {
 		canvas.style.marginTop=0; //Canvas starts at (0,0) if browser becomes smaller than canvas
 	}
 	
 	//Clears canvas
-	context.clearRect(0,0,canvas.width,canvas.height);
+	context.clearRect(0,0,cWidth,cHeight);
 
 	//Creates Platform
 	context.fillStyle = 'green';
-	platformHeight = canvas.height*0.35;
-	context.fillRect(0,canvas.height-platformHeight,canvas.width,platformHeight);
+	platformHeight = cHeight*0.35;
+	context.fillRect(0,cHeight-platformHeight,cWidth,platformHeight);
 
 	//Create Clouds
 	context.fillStyle = 'white';
 	cloudYPositions = [cHeight*0.2, cHeight*0.1, cHeight*0.3, cHeight*0.15];
-	
+	cloudXPositions = [cWidth*0.1, cWidth*0.4, cWidth*0.65, cWidth*0.95];
+
 	for (i=0;i<4;i++) {
-		cloudXPositions[i]-=cWidth*0.005;
+		cloudLeftMove[i]+=0.005;
 		
-		if (cloudXPositions[i] < (-cWidth*0.14)) {
-			cloudXPositions[i] = cWidth;
+		if ((cloudXPositions[i]-(cloudLeftMove[i]*cWidth)) < (-cWidth*0.14)) {
+			cloudLeftMove[i] = (cloudXPositions[i]-cWidth)/cWidth;
 		}
 
-		context.fillRect(cloudXPositions[i],cloudYPositions[i],canvas.width*0.15, canvas.width*0.07);
+		context.fillRect(cloudXPositions[i]-(cloudLeftMove[i]*cWidth),cloudYPositions[i],canvas.width*0.1, canvas.width*0.05);
 	}
+
+	//Create Player
+	context.fillStyle = 'red';
+	playerWidth = cWidth*0.05;
+	playerHeight = playerWidth;
+
+	if (playerYPos>0) {
+		yVel -= grav;
+		playerYPos += yVel;
+		document.body.onkeypress = null;
+	}
+
+	else {
+		yVel = 0;
+		playerYPos = 0;
+		document.body.onkeypress = function(e) {
+
+			if (e.keyCode==32) { //Space Bar was pressed
+				yVel = 10;
+				playerYPos += yVel;
+			}
+		}
+	}
+
+	context.fillRect(40,cHeight-platformHeight-playerHeight-playerYPos, playerWidth, playerHeight);
 }
 
 animate();
+
+document.body.onkeypress = function(e) {
+
+	if (e.keyCode==32) { //Space Bar was pressed
+		yVel = 10;
+		playerYPos += yVel;
+	}
+}
